9f3add6fd74f73f02e85a87c22f090589c106473
Bernylujan <bernylujan@gmail.com>
2026-02-24 21:25:20 -0600

    funcion banxico creada

---
+++ src/api/fetcher.js
@@ -0,0 +1,16 @@
+import { CONFIG } from "../config.js";
+
+async function obtenerTipoCambio() {
+    const hoy = new Date().toISOString().split('T')[0];
+    const url = `https://www.banxico.org.mx/SieAPIRest/service/v1/series/${CONFIG.banxico.serieTC}/datos/2015-01-01/${hoy}?token=${CONFIG.banxico.token}`;
+    const res = await fetch(url);
+    if (!res.ok) throw new Error("Error en red");
+    const texto = await res.text();
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(texto, 'text/xml');
+    const nodos = xml.querySelectorAll('Obs');
+    return Array.from(nodos).map(obs => ({
+        fecha: obs.querySelector('fecha').textContent,
+        valor: obs.querySelector('dato').textContent
+    }));
+}
